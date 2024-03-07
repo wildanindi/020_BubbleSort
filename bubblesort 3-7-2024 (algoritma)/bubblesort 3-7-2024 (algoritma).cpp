b3fbeca9f7f6a9d11c24f848eb6ae78f55ca1a2c
wildanindi <wildanindi2@gmail.com>
2024-03-07 20:19:46 +0700

    add element at index j is greater than the element at index j + 1, and swap the two elements

--- bubblesort 3-7-2024 (algoritma)/bubblesort 3-7-2024 (algoritma).cpp
+++ bubblesort 3-7-2024 (algoritma)/bubblesort 3-7-2024 (algoritma).cpp
@@ -34,3 +34,9 @@ void bubblesortarray() {    //prosedur untuk mengurutkan array dengan metode bub
 
     do {
         for (int j = 0; j <= n - 1 - pass; j++) {   // step 2
+            if (arr[j] > arr[j + 1]) {              // step 3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] + temp;
+            }
+        }
