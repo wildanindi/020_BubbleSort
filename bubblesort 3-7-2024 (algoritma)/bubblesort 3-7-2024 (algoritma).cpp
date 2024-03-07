70e424c4b5cd1d00003558964f5f546daa295a21
wildanindi <wildanindi2@gmail.com>
2024-03-07 20:15:16 +0700

    add looping dengan menggunakan while

--- bubblesort 3-7-2024 (algoritma)/bubblesort 3-7-2024 (algoritma).cpp
+++ bubblesort 3-7-2024 (algoritma)/bubblesort 3-7-2024 (algoritma).cpp
@@ -7,4 +7,25 @@ int arr[20];    // Deklarasi array a dengan ukuran 20
 int n;          // deklarasi variabel n untuk menyimpan banyaknya elemen pada array 
 
 void input() {  // procedur untuk input
+    while (true) {  // looping
+        cout << "Masukkan banyaknya elemen pada array : ";  // output ke layar
+        cin >> n;       // input dari pengguna
+        if (n <= 20)    // jika n kurang dari atau sama dengan 20
+            break;      //keluar dari loop
+        else {          // jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";    // output ke layar
+        }
+    }
+
+    cout << endl;                               // output bari kosong
+    cout << "=================" << endl;        // output ke layar
+    cout << "Masukkan Elemen Array" << endl;    // output ke layar
+    cout << "=================" << endl;        // output ke layar
+
+    for (int i = 0; i < n; i++) {       // looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": ";  //output ke layar
+        cin >> arr[i];  //input dari pengguna
+    }
+
+}
 
