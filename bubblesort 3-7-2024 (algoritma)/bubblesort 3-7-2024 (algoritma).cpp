e2623c115dddced9d487ad09a969e58402e49482
wildanindi <wildanindi2@gmail.com>
2024-03-07 20:14:23 +0700

    menambah variabel dan prosedur input

--- bubblesort 3-7-2024 (algoritma)/bubblesort 3-7-2024 (algoritma).cpp
+++ bubblesort 3-7-2024 (algoritma)/bubblesort 3-7-2024 (algoritma).cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
 
+
+int arr[20];    // Deklarasi array a dengan ukuran 20
+int n;          // deklarasi variabel n untuk menyimpan banyaknya elemen pada array 
+
+void input() {  // procedur untuk input
 
