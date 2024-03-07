38f98d3672e951de50508f0ed9ac3decd1dc41f9
wildanindi <wildanindi2@gmail.com>
2024-03-07 20:22:09 +0700

    and void display agar project yang di atas bisa di tampilkan

--- bubblesort 3-7-2024 (algoritma)/bubblesort 3-7-2024 (algoritma).cpp
+++ bubblesort 3-7-2024 (algoritma)/bubblesort 3-7-2024 (algoritma).cpp
@@ -51,3 +51,22 @@ void bubblesortarray() {    //prosedur untuk mengurutkan array dengan metode bub
 
     } while (pass <= n - 1); // step 5
 }
+
+void display() {
+    cout << endl;
+    cout << "=====================================" << endl;    // output ke layar
+    cout << "Element Array yang telah tersusun" << endl;        // output ke layar
+    cout << "=====================================" << endl;    // output ke layar
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j];
+        if (j < n - 1) {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+
+}
